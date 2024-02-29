3921e33ed0433250ea08ba8f2b9a4008551dbea6
Phanith NY <ny.phanith.fe@gmail.com>
2024-02-29 22:01:43 +0700

    Removed squircle

--- Sources/EasyUI/Helpers/UIView+Extensions.swift
+++ Sources/EasyUI/Helpers/UIView+Extensions.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 extension UIView {
-  public final func squircle(_ cornerRadius: CGFloat = 10, masksToBounds: Bool = true) {
-    if #available(iOS 13.0, *) {
-      layer.cornerCurve = .continuous
-    }
-    layer.cornerRadius = cornerRadius
-    layer.masksToBounds = masksToBounds
-  }
+//  public final func squircle(_ cornerRadius: CGFloat = 10, masksToBounds: Bool = true) {
+//    if #available(iOS 13.0, *) {
+//      layer.cornerCurve = .continuous
+//    }
+//    layer.cornerRadius = cornerRadius
+//    layer.masksToBounds = masksToBounds
+//  }
   
   public final func renderBorder(_ borderWidth: CGFloat = 0.66,
                                  borderColor: UIColor = .defaultBorderColor) {
